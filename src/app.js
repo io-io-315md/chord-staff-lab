a6922e1b6198f012fc82276ec41dbea7c95be481
io-io-315md <286819265+io-io-315md@users.noreply.github.com>
2026-08-08 18:41:36 +0900

    fix analysis preview layout

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ import {
   noteFromStaffPosition,
   parseChordSymbol,
   rankChordCandidates,
-} from './music-theory.js?v=9';
+} from './music-theory.js?v=10';
 import {
   pointerYInSvg,
   renderGrandStaff,
   renderNoteReadingStaff,
   staffPositionFromY,
-} from './staff-renderer.js?v=9';
+} from './staff-renderer.js?v=10';
 
 const chordForm = document.querySelector('#chord-form');
 const chordRoot = document.querySelector('#chord-root');
